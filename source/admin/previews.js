6c1ee5cccadf7943456400eef852336f5679feb4
AttentionNJ <109901858+AttentionNJ@users.noreply.github.com>
2022-07-25 01:27:10 +0800

    Update previews.js

--- source/admin/previews.js
+++ source/admin/previews.js
@@ -70,7 +70,7 @@ const NavigationPreview = ({ entry }) => {
     <header className="header-primary">
       <div className="logo">
         <a href="/">
-          <img src="/images/logo.svg" alt="Kaldi"/>
+          <img src="https://lh3.googleusercontent.com/pYYQaA0teiO1hCSOR6L2etf_no2b0ehsOVxIsqJ_RbrQPgagSI3IVtUszbl5uVK-Guf_LzeqPDV5_1gUQjCLtMH0fwaN-TraEpwVWBwUGHPy_NqZo4rGfbzMs_7Sx_5Z-PbxSYHC41XSmZCv9hrwl95dF6CSSV8zqoW0wWF4o-wwIYPOSMnYZB9XsW6HOp9VO5JRRYMlX4s62Z2o0rmpHbsPzrRr4b_InI3T3x5MhRl3GfrqZ_LOJdB_4WoiWBjqqcNDl133wDaF--Z5C_pzaD32z3WPs9aZ0PDHLPpptX9qqiqEbREeLQ3WIrCzyNx7hysT5dVQDgRaVFSAa_6uXOiOOEnaGcYDbMSPIW-6wDLNp3X8ht2ZL9yuqWlc5vMfMNkmYpckCubKxOD2WhsmZ-2weKvmOOo949DBHSbqPvab1QGTy6Ny2KiZs3kmO38q-qeg_FriJ5CgK8YdvLe-IQfmBxzgOT_kVQdWvoIrPG7-LPtHydNIbjcEGyc1zt44GadbFMo4Syxk9Mb9HwxO3uMDe1EFphSNyBTLumcjTdBA-TKtmu1egVeK7LoMSuhigcGgVpn0oacHgsaQ61o3eCTFBGDYBsWvnWapyxHbc6qkVM37PbNpEaVb1k5Smnn7yuoG41Y8fIEx-4qmDQNphMJ8-WJArSyFRNm66U8ShIrL84rXIYij59FatT4o3PNxlLVkLufj5uH7JPsW9Gw0086eOMq2f_QxpGrnp0EW85Ybp5LblPtqWDR8R6BshZdheLD4vgt5SFftGKwD_RRfFTGOFFzNNnCQeA=w371-h158-no?authuser=1" alt="Kaldi"/>
         </a>
       </div>
       <a className="nav-button">
